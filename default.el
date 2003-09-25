726c33eab1fece7d54a52012964688eab427784f
Igor B. Poretsky <poretsky@mlbox.ru>
2003-09-25 23:02:07 +0000

    Added some autoloads and key definitions.

--- default.el
+++ default.el
@@ -2,4 +2,11 @@
 ;; Rbook
 (autoload 'rbook-read-text "rbook" nil t)
 (autoload 'rbook-read-bookmark "rbook" nil t)
-(global-set-key (kbd "C-c r") 'rbook-read-bookmark)
+(autoload 'rbook-make-audio-book "rbook" nil t)
+(autoload 'rbook-show-time "rbook" nil t)
+(autoload 'rbook-customize "rbook" nil t)
+(global-set-key (kbd "C-c b") 'rbook-read-bookmark)
+(global-set-key (kbd "C-c r") 'rbook-read-text)
+(global-set-key (kbd "C-c a") 'rbook-make-audio-book)
+(global-set-key (kbd "C-c t") 'rbook-show-time)
+(global-set-key (kbd "C-c c") 'rbook-customize)
